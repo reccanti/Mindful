45761df7143b0f84404a295bd3a6abd8f00af933
Ben Wilcox <reccanti@gmail.com>
2017-05-08 18:23:00 -0400

    Unwind from views
    Allows the user to unwind to the Scene Builder from any of the
    offshoot screens

--- Mindful/LocationTableVC.swift
+++ Mindful/LocationTableVC.swift
@@ -1,5 +1,5 @@
 //
-//  SceneBuilderVC.swift
+//  LocationTableVC.swift
 //  Mindful
 //
 //  Created by Benjamin Wilcox on 5/8/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SceneBuilderVC: UITableViewController {
+class LocationTableVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,15 +27,15 @@ class SceneBuilderVC: UITableViewController {
 
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -82,24 +82,12 @@ class SceneBuilderVC: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
-    */
-    
-    
-    /**
-     * Unwind to the scene builder without
-     * selecting anythins
-     */
-    @IBAction func cancelToSceneBuilder(segue: UIStoryboardSegue) {
-        //
-        print("cancel")
     }
 
 }
